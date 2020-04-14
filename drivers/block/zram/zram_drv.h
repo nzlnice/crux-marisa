c4e04c8563d1c76f22498d7351ea7f7e0fe898eb
Martin Liu <liumartin@google.com>
2020-04-14 22:22:58 +0800

    Revert "zram: make deduplication feature optional"
    This reverts commit be0c36ce988836c7c9282e2892f3a213eb2b79e3.
    
    Reason for revert: revert non upstream code
    Bug: 153969530
    Test: memory stress test
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: I65a52eae96fead55d48a70ec51f842940ddf08a7
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -117,6 +117,7 @@ struct zram {
 	 * zram is claimed so open request will be failed
 	 */
 	bool claim; /* Protected by bdev->bd_mutex */
+	struct file *backing_dev;
 #ifdef CONFIG_ZRAM_WRITEBACK
 	struct file *backing_dev;
 	spinlock_t wb_limit_lock;
@@ -131,4 +132,6 @@ struct zram {
 	struct dentry *debugfs_dir;
 #endif
 };
+
+void zram_entry_free(struct zram *zram, struct zram_entry *entry);
 #endif
