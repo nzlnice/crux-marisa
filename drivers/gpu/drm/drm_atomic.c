e03a0ab8a5e56d49f532baa6f44def5d4b2ba0ae
John Galt <johngaltfirstrun@gmail.com>
2023-06-30 21:48:42 -0400

    defconfig: regen for no userspace perf

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2248,6 +2248,14 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		//7000ms covers long scrolls after input boosting is no longer used
+		if (time_before(jiffies, last_input_time + msecs_to_jiffies(7000))) {
+			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+			devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
+		}
+	}
+
 	drm_modeset_acquire_init(&ctx, 0);
 
 	state = drm_atomic_state_alloc(dev);
